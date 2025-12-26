0642e4839c3139a43f1ab857a093e5b61f4e3043
exml_team007 exml_team007 <exml_team007@conduit.hpc.uni-saarland.de>
2025-12-27 00:26:29 +0100

    add: isic training results

--- execute.sh
+++ execute.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Root of your project checkout on the cluster
 PROJECT_ROOT="${PROJECT_ROOT:-/home/exml_team007/certified-attribution-medical-imaging}"
-
-# Conda Python inside your env (adjust env name if different)
 CONDA_PYTHON_BINARY_PATH="${CONDA_PYTHON_BINARY_PATH:-/home/exml_team007/miniconda3/envs/certified-attribution-medical-imaging/bin/python}"
-
-# Default script to run (can be overridden by passing args in the .sub file)
 PYTHON_SCRIPT="${PYTHON_SCRIPT:-train_isic_server.py}"
 
 cd "$PROJECT_ROOT"
